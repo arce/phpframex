4337fe0ed9ee980ce11e3fb0d8a50c59443782b7
Armando Arce <armando.arce@gmail.com>
2019-04-02 17:31:24 -0600

    Update

--- PHPFramex.php
+++ PHPFramex.php
@@ -1,5 +1,11 @@
 <?php
-    
+
+$sessions = [];
+$cookies = [];
+$redirect = null;
+$type = 'html';
+$accept = [];
+
 /**
  * @method static Macaw get(string $route, Callable $callback)
  * @method static Macaw post(string $route, Callable $callback)
@@ -14,6 +20,7 @@ class Route {
   public static $methods = array();
   public static $callbacks = array();
   public static $maps = array();
+  public static $formats = array();
   public static $patterns = array(
       ':number' => '[0-9]+',
       ':alpha' => '[A-Za-z]+',
@@ -31,26 +38,35 @@ class Route {
         $maps = array_map('strtoupper', $params[0]);
         $uri = strpos($params[1], '/') === 0 ? $params[1] : '/' . $params[1];
         $callback = $params[2];
+         if (isset($params[3]))
+          $formats = $params[3];
+        else
+          $formats = 'html';
     } else {
         $maps = null;
         $uri = strpos($params[0], '/') === 0 ? $params[0] : '/' . $params[0];
         $callback = $params[1];
+        if (isset($params[2]))
+          $formats = $params[2];
+        else
+          $formats = 'html';
     }
 
     array_push(self::$maps, $maps);
     array_push(self::$routes, $uri);
+    array_push(self::$formats, $formats);
     array_push(self::$methods, strtoupper($method));
     array_push(self::$callbacks, $callback);
   }
 
-  public static function resource($uri,$controller) {
-    self::get($uri, $controller.'@index');
-    self::get($uri.'/create', $controller.'@create');
-    self::post($uri, $controller.'@store');
-    self::get($uri.'/(:string)', $controller.'@show');
-    self::get($uri.'/(:string)/edit',$controller.'@edit');
-    self::put($uri.'/(:string)',$controller.'@update');
-    self::delete($uri.'/(:string)',$controller.'@destroy');
+  public static function resource($uri,$controller,$format='html') {
+    self::get($uri, $controller.'@index',$format);
+    self::get($uri.'/create', $controller.'@create',$format);
+    self::post($uri, $controller.'@store',$format);
+    self::get($uri.'/(:string)', $controller.'@show',$format);
+    self::get($uri.'/(:string)/edit',$controller.'@edit',$format);
+    self::put($uri.'/(:string)',$controller.'@update',$format);
+    self::delete($uri.'/(:string)',$controller.'@destroy',$format);
   }
 
   /**
@@ -68,6 +84,8 @@ class Route {
 	global $sessions;
 	global $cookies;
 	global $redirect;
+	global $type;
+	global $accept;
 
     $content = self::_dispatch();
     
@@ -84,9 +102,29 @@ class Route {
 	  else
 	    setcookie($key,$value);
 	}
-	
-	if ($content!=null)
-	  echo $content;
+
+	if ($content!=null) {
+	  if (in_array('text/html',$accept)||
+	      in_array('*/*',$accept)) {
+	    if (is_array($content)) {
+		  header('Content-Type: text/html');
+	      echo to_html($content);
+	    } else echo $content;
+	  } else if (in_array('application/json',$accept)) {
+		if (is_array($content)) {
+		  header('Content-Type: application/json');
+	      echo json_encode($content);
+	    } else echo $content;
+	  } else if (in_array('application/xml',$accept)) {
+		if (is_array($content)) {
+		  header('Content-Type: application/xml');
+		  $str = '<data></data>';
+	      $xml = new SimpleXMLElement($str,0,false);
+          to_xml($xml, $content);
+          echo $xml->asXML();
+        } else echo $content;
+	  }
+	}
 
 	if ($redirect!=null)
 	  header('Location: ' . $redirect, true, 303);
@@ -96,9 +134,13 @@ class Route {
    * Runs the callback for the given request
    */
   public static function _dispatch(){
+	global $type;
+	global $accept;
+	
     $uri = parse_url($_SERVER['REQUEST_URI'], PHP_URL_PATH);
     $method = $_SERVER['REQUEST_METHOD'];
-
+    $type = $_SERVER['CONTENT_TYPE'];
+    $accept = explode(",",$_SERVER['HTTP_ACCEPT']);
     $searches = array_keys(static::$patterns);
     $replaces = array_values(static::$patterns);
 
@@ -112,7 +154,11 @@ class Route {
       foreach ($route_pos as $route) {
 
         // Using an ANY option to match both GET and POST requests
-        if (self::$methods[$route] == $method || self::$methods[$route] == 'ANY' || (!empty(self::$maps[$route]) && in_array($method, self::$maps[$route]))) {
+        if (self::$methods[$route] == $method || 
+            self::$methods[$route] == 'ANY' || 
+           (!empty(self::$maps[$route]) && 
+           in_array($method, self::$maps[$route]))) {
+	
           $found_route = true;
 
           // If route is not an object
@@ -128,7 +174,7 @@ class Route {
             $segments = explode('@',$last);
 
             require_once('controllers/'.$segments[0].'.php');
-              
+            
             // Instanitate controller
             $controller = new $segments[0]();
 
@@ -153,11 +199,26 @@ class Route {
         }
 
         if (preg_match('#^' . $route . '$#', $uri, $matched)) {
-          if (self::$methods[$pos] == $method || self::$methods[$pos] == 'ANY' || (!empty(self::$maps[$pos]) && in_array($method, self::$maps[$pos]))) {
+          if (self::$methods[$pos] == $method || 
+              self::$methods[$pos] == 'ANY' || 
+             (!empty(self::$maps[$pos]) && 
+             in_array($method, self::$maps[$pos]))) {
+		
             $found_route = true;
+            parse_str(file_get_contents("php://input"), $_REQUEST);
+            
+            $data = [];
 
+            if ($type=='application/json') {
+               $data = json_decode(file_get_contents('php://input'), True);
+            } else if ($type=='application/xml') {
+               $data = simplexml_load_string(file_get_contents('php://input'));
+            }
+            
             // Remove $matched[0] as [1] is the first parameter.
             array_shift($matched);
+            if ($data!=[])
+              $matched = array_merge(array($data),$matched);
 
             if (!is_object(self::$callbacks[$pos])) {
 
@@ -179,7 +240,7 @@ class Route {
               if (!method_exists($controller, $segments[1])) {
                 echo "controller and action not found";
               } else {
-                return call_user_func_array(array($controller, $segments[1]), $matched);
+                return call_user_func_array(array($controller,$segments[1]), $matched);
               }
 
               if (self::$halts) return;
@@ -212,6 +273,42 @@ class Route {
       call_user_func(self::$error_callback);
     }
   }
+}
+
+function to_xml(SimpleXMLElement $object, array $data) {   
+    foreach ($data as $key => $value) {
+        if (is_array($value)) {
+			if ($key == (int)$key) $key = "item";
+            $new_object = $object->addChild($key);
+            to_xml($new_object, $value);
+        } else {
+            if ($key != (int)$key) $key = "key_$key";
+            $object->addChild($key, $value);
+        }   
+    }   
+}   
+
+function to_html($array) {
+    // start table
+    $html = '<table>';
+    // header row
+    $html .= '<tr>';
+    foreach($array[0] as $key=>$value){
+            $html .= '<th>' . htmlspecialchars($key) . '</th>';
+        }
+    $html .= '</tr>';
+
+    // data rows
+    foreach( $array as $key=>$value){
+        $html .= '<tr>';
+        foreach($value as $key2=>$value2){
+            $html .= '<td>' . htmlspecialchars($value2) . '</td>';
+        }
+        $html .= '</tr>';
+    }
+
+    $html .= '</table>';
+    return $html;
 }
 
 function redirect($url, $statusCode = 303) {
@@ -690,10 +787,10 @@ abstract class Controller {
   
   public function index() {}
   public function create() {}
-  public function store() {}
+  public function store($request) {}
   public function show($id) {}
   public function edit($id) {}
-  public function update($id) {}
+  public function update($param1,$param2) {}
   public function destroy($id) {}
 }
 ?>
@@ -726,7 +823,7 @@ class Cookie {
 	return isset($_COOKIE[$key]);
   }
   
-  public static function queue($key,$value) {
+  public static function queue($key,$value,$time) {
 	global $cookies;
 	$cookies[$key] = $value;
   }
--- index.php
+++ index.php
@@ -1,9 +1,5 @@
 <?php
     session_start();
-    parse_str(file_get_contents("php://input"), $_REQUEST);
-    $sessions = [];
-    $cookies = [];
-    $redirect = null;
     require('PHPFramex.php');
     require('routes.php')
 ?>
