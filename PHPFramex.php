826a3dacf692a9f3f185c31da1d40a5859cc7e68
Armando Arce <armando.arce@gmail.com>
2024-02-28 15:14:14 -0600

    Update PHPFramex.php

--- PHPFramex.php
+++ PHPFramex.php
@@ -778,42 +778,42 @@ class Model {
 
   public static function all() {
     $qry = new Query();
-  	$qry->params['table'] = static::$table;
+    $qry->params['table'] = static::$table;
     return $qry->get();
   }
 
   public static function find($id) {
     $qry = new Query();
     $qry->params['table'] = static::$table;
-	$qry->find($id)
+    $qry->find($id);
     return $qry->get();
   }
 
   public static function where($field,$value) {
     $qry = new Query();
     $qry->params['table'] = static::$table;
-  	$qry->where($field,$value)
-	return $qry;
+    $qry->where($field,$value);
+    return $qry;
   }
 
   public static function create($item) {
-	$params = ['table' => static::$table];
-	DB::_insert($params,$item);
+    $params = ['table' => static::$table];
+    DB::_insert($params,$item);
   }
 
   public static function update($id,$item) {
     $params = ['table' => static::$table,'where'=>[["AND",$field,"=",$value]]];
-	DB::_update($params,$item);
+    DB::_update($params,$item);
   }
 
   public static function destroy($id) {
     $params = ['table' => static::$table,'where'=>[["AND",$field,"=",$value]]];
-	DB::_delete($params);
+    DB::_delete($params);
   }
 
   public function save() {
-	$item = [];
-	foreach (static::$columns as $k => $v)
+    $item = [];
+    foreach (static::$columns as $k => $v)
       $item[$v] = $this->{$v};
     if ($id==NULL)
       DB::_insert($params,$item);
